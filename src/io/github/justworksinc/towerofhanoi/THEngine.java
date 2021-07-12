93b051521e0084d1aff00bc374da0011486f1ed1
sy3c4ll <sy3c4ll@gmail.com>
2021-07-12 22:50:36 +0900

    fix UI and IO

--- src/io/github/justworksinc/towerofhanoi/THEngine.java
+++ src/io/github/justworksinc/towerofhanoi/THEngine.java
@@ -1,39 +1,48 @@
 package io.github.justworksinc.towerofhanoi;
 import java.awt.Graphics;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
+import java.awt.event.ComponentEvent;
+import java.awt.event.ComponentListener;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
-public class THEngine implements ActionListener,MouseListener{
+public class THEngine implements MouseListener,ComponentListener{
   public THEngine(){
     // TODO
   }
   public Graphics draw(){
-    // TODO
     return null;
+    // TODO
   }
-	@Override public void actionPerformed(ActionEvent e){
-		// TODO Auto-generated method stub
-		
-	}
+  public void reset(){
+    // TODO
+  }
+  public void setHeight(int height){
+    // TODO
+  }
 	@Override public void mouseClicked(MouseEvent e){
 		// TODO Auto-generated method stub
-		
 	}
 	@Override public void mouseEntered(MouseEvent e){
 		// TODO Auto-generated method stub
-		
 	}
 	@Override public void mouseExited(MouseEvent e){
 		// TODO Auto-generated method stub
-		
 	}
 	@Override public void mousePressed(MouseEvent e){
 		// TODO Auto-generated method stub
-		
 	}
 	@Override public void mouseReleased(MouseEvent e) {
 		// TODO Auto-generated method stub
-		
 	}
+  @Override public void componentHidden(ComponentEvent e){
+    // TODO Auto-generated method stub
+  }
+  @Override public void componentMoved(ComponentEvent e){
+    // TODO Auto-generated method stub
+  }
+  @Override public void componentResized(ComponentEvent e){
+    // TODO Auto-generated method stub
+  }
+  @Override public void componentShown(ComponentEvent e){
+    // TODO Auto-generated method stub
+  }
 }
--- src/io/github/justworksinc/towerofhanoi/THScreen.java
+++ src/io/github/justworksinc/towerofhanoi/THScreen.java
@@ -2,18 +2,23 @@ package io.github.justworksinc.towerofhanoi;
 import java.awt.BorderLayout;
 import java.awt.Container;
 import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import javax.swing.JButton;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 public class THScreen extends JPanel{
 	private static final long serialVersionUID=-2327290491673766502L;
-	private THEngine engine;
 	private Container towers,buttons;
 	private JButton newgame,setheight,quit;
-	public THScreen(THEngine e){
-		engine=e;towers=new Container();buttons=new Container();newgame=new JButton("New Game");setheight=new JButton("Set Height");quit=new JButton("Quit");
-		addMouseListener(e);newgame.addActionListener(e);setheight.addActionListener(e);quit.addActionListener(e);
+	public THScreen(THEngine thengine){
+		towers=new Container();buttons=new Container();newgame=new JButton("New Game");setheight=new JButton("Set Height");quit=new JButton("Quit");
+		addMouseListener(thengine);addComponentListener(thengine);
+		newgame.addActionListener(new ActionListener(){@Override public void actionPerformed(ActionEvent e){thengine.reset();}});
+		setheight.addActionListener(new ActionListener(){@Override public void actionPerformed(ActionEvent e){while(true)try{thengine.setHeight(Integer.parseInt(JOptionPane.showInputDialog(towers,"Enter a new height","4")));break;}catch(Exception ex){JOptionPane.showMessageDialog(towers,"You must enter a positive integer","Invalid number",JOptionPane.ERROR_MESSAGE);}}});
+		quit.addActionListener(new ActionListener(){@Override public void actionPerformed(ActionEvent e){System.exit(0);}});
 		setLayout(new BorderLayout());buttons.setLayout(new GridLayout(1,3));
-		towers.paint(e.draw());add(newgame);buttons.add(setheight);buttons.add(quit);
+		towers.paint(thengine.draw());buttons.add(newgame);buttons.add(setheight);buttons.add(quit);
 		add(towers,BorderLayout.PAGE_START);add(buttons,BorderLayout.PAGE_END);
 	}
 }
